9e3814d26e536572ba0cb3db8d2306a61383b55f
Brian Hudson <brian.hudson@flexential.com>
2021-12-16 07:31:16 -0500

    Commit to restart branch

--- main.py
+++ main.py
@@ -1,17 +1,5 @@
-from src.food_processor import *
-from src.sheets_updater import *
+import datetime
+now = datetime.datetime.now()
 
-# workout_input = input_exercise()
-# workout_sheet = SheetsUpdater(workout_sheet_name)
-#
-# for index in workout_input:
-#     workout_sheet.add_row(entry_type='workout', add_object=index)
-# print(workout_sheet.get_sheets())
-#
-# print(food_sheet.get_sheets())
+print(f"New Beginnings! {now}")
 
-tracker_user = input("Which user? ")
-tracker_user = UserConfig(tracker_user)
-tracker_user.fetch_user_details()
-print(tracker_user.get_user_details())
-exercise_sheet = SheetsUpdater(tracker_user, row_type='exercise')
