c87f4369d957fdf3814f606300b20be6a9a93c2e
Brian Hudson <github@selfdestroyedindustries.com>
2021-12-16 22:13:02 -0500

    I need to sleep

--- main.py
+++ main.py
@@ -1,21 +1,39 @@
 from simple_term_menu import TerminalMenu
 import datetime
 import src.user_class
+import src.run_digester
+import src.food_digester
 
 now = datetime.datetime.now()
 
+in_loop = True
+
 
 def main():
+    results = []
     email = input("What is your email? ")
     user = src.user_class.UserClass(email)
     user.check_login()
     user_profile = user.get_profile()
     print(f"Welcome back {user_profile[1]}!")
     print(f"Last Login: {user_profile[3]}")
-    options = ["Display", "Add", "Update", "Delete"]
-    terminal_menu = TerminalMenu(options)
-    menu_entry_index = terminal_menu.show()
-    print(f"You have selected {menu_entry_index}!")
+
+    while in_loop:
+        food_digester = src.food_digester.FoodClass(now)
+        options = ["Display", "Add", "Update", "Delete"]
+        terminal_menu = TerminalMenu(options)
+        menu_index = terminal_menu.show()
+
+        if options[menu_index] == 'Display':
+            print(results)
+        if options[menu_index] == 'Add':
+            results.append(food_digester.get_food_input())
+            print(results)
+        if options[menu_index] == 'Update':
+            print("Update!")
+        if options[menu_index] == 'Delete':
+            for i in range(len(results)):
+                print(results[i])
 
 
 if __name__ == "__main__":
---
+++ src/food_digester.py
@@ -0,0 +1,49 @@
+import requests
+from src.environs import *
+
+
+class FoodClass:
+    def __init__(self, now):
+        self.time = now
+        self.return_results = []
+        self.query_results = {}
+        self.endpoint = "https://trackapi.nutritionix.com/v2/natural/nutrients/"
+        self.req_json = {}
+
+    def get_food_input(self):
+        query = input("What do you shove down your gullet today? ")
+        headers = {
+            'Content-Type': 'application/json',
+            'x-app-id': appid,
+            'x-app-key': apikey
+        }
+        query_json = {
+            'query': query,
+        }
+        try:
+            req = requests.post(url=self.endpoint, json=query_json, headers=headers)
+        except requests.exceptions.RequestException as exception:
+            raise SystemExit(exception)
+        self.req_json = req.json()
+        food_id = 0
+        total_cal = 0
+        for index, food_list in self.req_json.items():
+            for item in food_list:
+                print("----")
+                print(f"Name: {item['food_name']}")
+                print(f"Quantity: {item['serving_qty']}")
+                print(f"Weight (g): {item['serving_weight_grams']}")
+                print(f"Calories: {item['nf_calories']}")
+                item_cal = int(item['nf_calories'])
+                self.query_results[food_id] = {
+                    "Food": f"{item['food_name']}",
+                    "Calories": f"{item['nf_calories']}",
+                    "Date": f"{self.time.strftime('%Y/%m/%d')}",
+                    "Time": f"{self.time.strftime('%H:%M')}"
+                }
+                self.return_results.append(self.query_results[food_id])
+                total_cal = total_cal + item_cal
+                food_id += 1
+            print("-----------------")
+            print(f"Total Calories Consumed: {total_cal}")
+        return self.return_results
