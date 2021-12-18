d5de9571ff32aed6eea53e0bc564fa098ec18366
Brian Hudson <github@selfdestroyedindustries.com>
2021-12-18 11:26:15 -0500

    Adding Poetry

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Tracker1',
+    version='Alpha 1',
+    packages=[''],
+    url='http://github.com/bearhudson',
+    license='GNU',
+    author='brian',
+    author_email='reallybrianhudson@gmail.com',
+    description='Track1'
+)
