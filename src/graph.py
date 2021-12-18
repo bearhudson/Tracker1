c1506407281003f83a4591343a08a575bfaa6f4a
Brian Hudson <github@selfdestroyedindustries.com>
2021-12-18 15:06:32 -0500

    First Graph Version

--- src/graph.py
+++ src/graph.py
@@ -1,61 +1,35 @@
-import matplotlib.dates as dates
 import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
-from datetime import datetime
 
 
 def report(report_query, report_type):
-    df = pd.DataFrame(report_query)
     if report_type == 'food':
-        food_report(report_query, df)
+        food_report(report_query)
     elif report_type == 'exercise':
         exercise_report(report_query)
 
 
-def exercise_report(report_query):
-    r_date = []
-    r_activity = []
-    r_duration = []
-    r_calories = []
-    for rep in report_query:
-        r_date.append(rep[0])
-        r_activity.append(rep[1])
-        r_duration.append(rep[2])
-        r_calories.append(rep[3])
-    format_date = [datetime.strftime(d, '%m/%d') for d in r_date]
-    x = np.array(r_date)
-    y = np.array(r_calories)
-    plt.xlabel('Date')
-    plt.ylabel('Calories')
-    plt.title("Calorie Expenditure by Day")
-    plt.plot(x, y)
-    testing(query=report_query)
-    plt.show()
-
-
 def food_report(report_query):
-    r_date = []
-    r_food_name = []
-    r_quantity = []
-    r_calories = []
-    for rep in report_query:
-        r_date.append(rep[0])
-        r_food_name.append(rep[1])
-        r_quantity.append(rep[2])
-        r_calories.append(rep[3])
-    format_date = [datetime.strftime(d, '%m/%d') for d in r_date]
-    x = np.array(format_date)
-    y = np.array(r_calories)
-    plt.xlabel('Date')
-    plt.ylabel('Calories')
-    plt.title("Calorie Intake by Day")
-    plt.plot(x, y)
+    labels = ['date', 'name', 'quantity', 'calories', 'weight', 'user', 'uuid']
+    df = pd.DataFrame(report_query, columns=labels)
+    df.set_index('date')
+    plt.xlabel('Dates')
+    plt.ylabel('Numbers!')
+    plt.title("A graph with numbers")
+    plt.scatter(df['date'], df['calories'], label='Calories (k)', color='purple')
+    plt.scatter(df['date'], df['weight'], label='Weight (g)', color='orange')
+    plt.legend()
     plt.show()
 
 
-def testing(query):
+def exercise_report(report_query):
     labels = ['date', 'name', 'duration', 'calories', 'user', 'uuid']
-    df = pd.DataFrame(query, columns=labels)
-    print(df)
-
+    df = pd.DataFrame(report_query, columns=labels)
+    df.set_index('date')
+    plt.xlabel('Date')
+    plt.ylabel('Numbers')
+    plt.title("Calories by Date")
+    plt.scatter(df['date'], df['calories'], label='Calories (k)', color='green')
+    plt.scatter(df['date'], df['duration'], label='Weight (g)', color='red')
+    plt.legend()
+    plt.show()
